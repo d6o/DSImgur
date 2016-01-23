4f4d1e50546ac31865df400ff2f7f94ef5bcd646
Diego Martins de Siqueira <dieg0@live.com>
2016-01-23 20:08:07 -0200

    Detect urls patterns

---
+++ Imgur.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+
+'''
+	Diego Martins de Siqueira
+	A perfect Imgur Downloader
+
+
+	Urls Example:
+	-http://i.imgur.com/BoENDec.jpg
+	-https://imgur.com/a/5xK6z
+	-https://imgur.com/gallery/5xK6z
+	-https://imgur.com/WDn2pnD
+'''
+
+import sys
+import argparse
+import libs.mdownload.mdownload
+import re
+import urlparse
+
+class imgur:
+
+	domains = ['imgur.com', 'i.imgur.com']
+
+	def __init__(self, urlList, workers, folderPath):
+		self._urlList		= urlList
+		self._workers 		= workers
+		self._folderPath 	= folderPath
+		self.dlList			= []
+
+		self._prepareUrlList()
+
+	def _prepareUrlList(self):
+		album  	= re.compile('(\/)(gallery\/|a\/)(\w{5}?)')
+		single 	= re.compile('(\/[a-zA-Z\d]+)(\/)?')
+		direct 	= re.compile('(\/[a-zA-Z\d]+)(\.\w{3,4})')
+
+		for url in self._urlList:
+
+			parse = urlparse.urlparse(url)
+
+			if not parse.netloc in self.domains:
+				continue
+
+			if album.search(parse.path):
+				self.dlList.append(self._prepareAlbum(parse.path))
+				continue
+			if direct.search(parse.path):
+				self.dlList.append(self._prepareDirect(parse.path))
+				continue
+			if single.search(parse.path):
+				self.dlList.append(self._prepareSingle(parse.path))
+				continue
+
+	def _prepareAlbum(self, path):
+		if not path.endswith('/'):
+			path += '/'
+		path += 'zip'
+		return 'https://imgur.com'+path
+
+	def _prepareDirect(self, path):
+		return 'https://i.imgur.com'+path
+
+	def _prepareSingle(self, path):
+		if path.endswith('/'):
+			path = path[:-1]
+		return 'https://i.imgur.com'+path+'.jpg'
+
+	def download(self):
+		if len(self.dlList) <= 0:
+			return false
+		mdownload(self.dlList, self._workers, self._folderPath)
+
+
+def main(argv):
+	parser = argparse.ArgumentParser(
+		description = "A perfect Imgur Downloader.")
+	parser.add_argument("--threads", type=int, default=5, 
+		help="Number of parallel downloads. The default is 5.")
+	parser.add_argument("--output", type=str, default="downloads", 
+		help="Output folder")
+	parser.add_argument('urls', type=str, nargs='+',
+		help='URLs to be downloaded')
+
+	args = parser.parse_args()
+
+	try:
+		i = imgur(args.urls, args.threads, args.output)
+		result = i.download()
+
+		if not result:
+			print "no valid URL"
+
+		print 'Finished.'
+	except KeyboardInterrupt:
+		print 'Interrupt received, stopping downloads'
+
+	sys.exit()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
